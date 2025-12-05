4c2c278a1c3e1780cb34ad7b873c71b8f4f8857e
vlad15244 <87974193+vlad15244@users.noreply.github.com>
2025-12-06 01:33:34 +0300

    Add files via upload

--- vars/urls.py
+++ vars/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, add_recipe
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='recipe_list'),
+    path('new/', add_recipe, name='add_new')
 ]
--- vars/views.py
+++ vars/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 
 from .models import Recipe
-
+from .forms import RecipeForm
 
 def index(request):
     template = loader.get_template('vars/index.html')
@@ -11,3 +11,15 @@ def index(request):
     context = {'recipes': recipes}
     return HttpResponse(template.render(context, request))
 # Create your views here.
+
+
+def add_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_list')
+    else:
+        form = RecipeForm()
+    
+    return render(request, 'vars/add_recipe.html', {'form':form})
